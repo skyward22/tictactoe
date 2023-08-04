9106465c1f62ff669340aebb5a41d030fe88c2d8
Skyler Trent <skyler.trent93@gmail.com>
2023-08-03 20:59:40 -0600

    tiktactoe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const handleClick = (index) => {
+    if (board[index] || calculateWinner(board)) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = isXNext ? "X" : "O";
+
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <button className="square" onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  const winner = calculateWinner(board);
+
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else if (board.every((cell) => cell)) {
+    status = "It's a draw!";
+  } else {
+    status = `Next player: ${isXNext ? "X" : "O"}`;
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+function calculateWinner(board) {
+  const winLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winLines.length; i++) {
+    const [a, b, c] = winLines[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+
+  return null;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
     </div>
   );
 }
